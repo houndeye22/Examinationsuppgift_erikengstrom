395d8fcf6a26b48f8cb197f19b97a1678e92934e
Erik Engstrom <erik.engstrom@elev.ga.lbs.se>
2018-10-07 14:04:58 +0200

    skeppet får rändom färg när man klickar på space

--- Assets/ShipScript.cs
+++ Assets/ShipScript.cs
@@ -8,8 +8,10 @@ using UnityEngine;
 public class ShipScript : MonoBehaviour
 {
     //Functions och variabler
-    public SpriteRenderer rend;
-    public Color shipColor;
+    public SpriteRenderer rend1;
+    public SpriteRenderer rend2;
+    public Color shipColorFront;
+    public Color shipColorBase;
     public Transform shipLocation;
 
     [Range(-500, 500)]
@@ -19,6 +21,11 @@ public class ShipScript : MonoBehaviour
     public float shipRotateSpeed;
 
     public float timer;
+
+    public float shipColorRandom1;
+    public float shipColorRandom2;
+    public float shipColorRandom3;
+    public float shipColorFinal;
 
 
     void Start()
@@ -36,8 +43,8 @@ public class ShipScript : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0f, 0f, shipRotateSpeed * Time.deltaTime);
-            rend.color = shipColor;
-            rend.color = new Color(0f, 0f, 1f);
+            rend1.color = shipColorFront;
+            rend1.color = new Color(0f, 0f, 1f);
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -47,8 +54,19 @@ public class ShipScript : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0f, 0f, -shipRotateSpeed * Time.deltaTime);
-            rend.color = shipColor;
-            rend.color = new Color(0f, 1f, 0f);
+            rend1.color = shipColorFront;
+            rend1.color = new Color(0f, 1f, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shipColorRandom1 = Random.Range(0.1f, 1f);
+            shipColorRandom2 = Random.Range(0.1f, 1f);
+            shipColorRandom3 = Random.Range(0.1f, 1f);
+            shipColorFinal = shipColorRandom1 + shipColorRandom2 + shipColorRandom3;
+
+            shipColorBase = new Color(shipColorRandom1, shipColorRandom2, shipColorRandom3);
+            rend2.color = shipColorBase;
         }
 
         timer += 1 * Time.deltaTime;
