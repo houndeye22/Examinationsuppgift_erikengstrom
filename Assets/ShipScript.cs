be2a4f6ee6fed4203345fe355d2303b47fe165e1
Erik Engstrom <erik.engstrom@elev.ga.lbs.se>
2018-10-08 13:51:45 +0200

    fixade timern och skeppet spawnar på en random position

--- Assets/ShipScript.cs
+++ Assets/ShipScript.cs
@@ -17,20 +17,40 @@ public class ShipScript : MonoBehaviour
     [Range(-500, 500)]
     public int shipSpeed;
 
+    public int shipSpeedMin;
+    public int shipSpeedMax;
+
     [Range(-720, 720)]
-    public float shipRotateSpeed;
+    public float shipRotateSpeedLeft;
+
+    [Range(-720, 720)]
+    public float shipRotateSpeedRight;
 
     public float timer;
+    public int timerCount;
 
     public float shipColorRandom1;
     public float shipColorRandom2;
     public float shipColorRandom3;
-    public float shipColorFinal;
+
+
+    public int negativeSpawnX;
+    public int negativeSpawnY;
+    public int positiveSpawnX;
+    public int positiveSpawnY;
+    public int finalSpawnX;
+    public int finalSpawnY;
+    public Vector2 shipSpawnPos;
 
 
     void Start()
     {
+        shipSpeed = Random.Range(shipSpeedMin, shipSpeedMax);
 
+        finalSpawnX = Random.Range(negativeSpawnX, positiveSpawnX);
+        finalSpawnY = Random.Range(negativeSpawnY, positiveSpawnY);
+        shipSpawnPos = new Vector2(finalSpawnX, finalSpawnY);
+        transform.position = shipSpawnPos;
     }
 
 
@@ -42,7 +62,7 @@ public class ShipScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f, shipRotateSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, shipRotateSpeedLeft * Time.deltaTime);
             rend1.color = shipColorFront;
             rend1.color = new Color(0f, 0f, 1f);
         }
@@ -53,7 +73,7 @@ public class ShipScript : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0f, -shipRotateSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, -shipRotateSpeedRight * Time.deltaTime);
             rend1.color = shipColorFront;
             rend1.color = new Color(0f, 1f, 0f);
         }
@@ -63,13 +83,17 @@ public class ShipScript : MonoBehaviour
             shipColorRandom1 = Random.Range(0.1f, 1f);
             shipColorRandom2 = Random.Range(0.1f, 1f);
             shipColorRandom3 = Random.Range(0.1f, 1f);
-            shipColorFinal = shipColorRandom1 + shipColorRandom2 + shipColorRandom3;
 
             shipColorBase = new Color(shipColorRandom1, shipColorRandom2, shipColorRandom3);
             rend2.color = shipColorBase;
         }
 
         timer += 1 * Time.deltaTime;
-        print(string.Format("Timer: {0}", timer));
+        if (timer > timerCount)
+        {
+            timerCount += 1;
+            print(string.Format("Timer: {0}", timerCount));
+        }
+        
     }
 }
